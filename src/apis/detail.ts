f64c63cc992ef6fc89e83c2805ebc085cd0ab11b
Rogertheone <ln451920@dal.ca>
2025-07-30 13:54:52 +0800

    feat: implement hot ranking section with API integration and product rendering

--- src/apis/detail.ts
+++ src/apis/detail.ts
@@ -13,3 +13,26 @@ export const getDetail = (id: any) => {
     }
   })
 }
+
+/**
+ * 获取热榜商品
+ * @param {Number} id - 商品id
+ * @param {Number} type - 1代表24小时热销榜 2代表周热销榜
+ * @param {Number} limit - 获取个数
+ */
+interface FetchHotGoodsParams {
+  id: number
+  type: number
+  limit?: number
+}
+
+export const fetchHotGoodsAPI = ({ id, type, limit = 3 }: FetchHotGoodsParams) => {
+  return request({
+    url: '/goods/hot',
+    params: {
+      id,
+      type,
+      limit
+    }
+  })
+}
--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -2,7 +2,7 @@
 import { getDetail } from '@/apis/detail';
 import { ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router'
-
+import DetailHot from './components/DetailHot.vue';
 
 
 const goods = ref({})
@@ -120,6 +120,8 @@ onMounted(() => getGoods())
             </div>
             <!-- 24热榜+专题推荐 -->
             <div class="goods-aside">
+              <DetailHot />
+              <DetailHot />
 
             </div>
           </div>
